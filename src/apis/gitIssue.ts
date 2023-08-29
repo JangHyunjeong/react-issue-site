75ddecd741e7aec3cedfd42bc907b8c283ad9693
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-08-30 00:39:55 +0900

    feat: DetailPage에 필요한 데이터 전달

--- src/apis/gitIssue.ts
+++ src/apis/gitIssue.ts
@@ -4,3 +4,8 @@ export const getIssueRequest = async (page: number) => {
   const { data } = await Instance.get(`issues?page=${page}`)
   return data
 }
+
+export const getIssueOneRequest = async (issueNumber: number) => {
+  const { data } = await Instance.get(`issues/${issueNumber}`)
+  return data
+}
--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,8 +1,75 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { getIssueOneRequest } from '../apis/gitIssue'
+
+interface userType {
+  login: string
+  avatar_url: string
+}
+
+interface issueType {
+  id: number
+  number: number
+  title: string
+  user: userType
+  created_at: string
+  comments: string
+  body: string
+}
+
 function DetailPage() {
+  const params = useParams()
+  const issueNumber = Number(params.id)
+  const [issueData, setIssueData] = useState<issueType>({
+    id: 0,
+    number: 0,
+    title: '',
+    user: {
+      login: '',
+      avatar_url: '',
+    },
+    created_at: '',
+    comments: '',
+    body: '',
+  })
+
+  useEffect(() => {
+    getIssueOneRequest(issueNumber).then((res) => {
+      setIssueData(res)
+    })
+  }, [issueNumber])
+
   return (
-    <>
+    <section>
       <h2>DetailPage</h2>
-    </>
+
+      <div>
+        <header>
+          <h3>
+            <span>{issueData.number}</span>
+            <strong>{issueData.title}</strong>
+          </h3>
+        </header>
+        <div>
+          <dl>
+            <dt>작성자 : </dt>
+            <dd>
+              <img src={issueData.user.avatar_url}></img> {issueData.user.login}
+            </dd>
+          </dl>
+          <dl>
+            <dt>작성일 : </dt>
+            <dd>{issueData.created_at}</dd>
+          </dl>
+          <dl>
+            <dt>코멘트 수 : </dt>
+            <dd>{issueData.comments}</dd>
+          </dl>
+        </div>
+      </div>
+
+      <div>{issueData.body}</div>
+    </section>
   )
 }
 export default DetailPage
