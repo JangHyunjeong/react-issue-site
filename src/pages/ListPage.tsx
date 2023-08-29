5f37d1ce847cca4a4c6fd28c2aebd650a8991b07
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-08-29 23:20:57 +0900

    feat: ListPage에 필요한 데이터 불러옴

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from 'react'
+import { getIssueRequest } from '../apis/gitIssue'
+
+interface userType {
+  login: string
+}
+
+interface issueListType {
+  id: number
+  number: number
+  title: string
+  user: userType
+  created_at: string
+  comments: string
+}
+
 function ListPage() {
+  const [issueList, setIssueList] = useState<issueListType[]>([])
+  const [issuePage] = useState<number>(1)
+
+  useEffect(() => {
+    getIssueRequest(issuePage).then((res) => {
+      setIssueList(res)
+    })
+  }, [])
+
   return (
     <>
       <h2>ListPage</h2>
+
+      {issueList.map((issue) => {
+        return (
+          <div key={issue.id}>
+            <div>{issue.number}</div>
+            <div>{issue.title}</div>
+            <div>{issue.user.login}</div>
+            <div>{issue.created_at}</div>
+            <div>{issue.comments}</div>
+          </div>
+        )
+      })}
     </>
   )
 }
