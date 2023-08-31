36553c9c5ed68476aa8ce1c76e3854b8d25a5d3e
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-08-31 14:23:15 +0900

    style: GlobalStyle, DefaultTheme 설정, 스타일 상수 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { styled } from 'styled-components'
 import GlobalHeader from './components/common/GlobalHeader'
 import PageRouter from './pages/PageRouter'
 
+import { styled, ThemeProvider } from 'styled-components'
+import { Theme } from './styles/base/DefaultTheme'
+import GlobalStyle from './styles/base/GlobalStyles'
+
 function App() {
   return (
-    <GlobalLayout>
-      <GlobalHeader />
-      <PageRouter />
-    </GlobalLayout>
+    <ThemeProvider theme={Theme}>
+      <GlobalLayout>
+        <GlobalStyle />
+        <GlobalHeader />
+        <PageRouter />
+      </GlobalLayout>
+    </ThemeProvider>
   )
 }
 export default App
---
+++ src/styles/constants/fontSizes.ts
@@ -0,0 +1,13 @@
+export interface fontSizes {
+  large: string
+  medium: string
+  default: string
+  small: string
+}
+
+export const fontSizes = {
+  large: '36px',
+  medium: '20px',
+  default: '16px',
+  small: '12px',
+}
